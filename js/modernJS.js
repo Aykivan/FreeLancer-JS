cd1e08d18d57e62be667fdf889c7241749d593e4
Айкин Иван <ivan.aykin@mail.ru>
2023-06-16 10:26:46 +0700

    added practice from modern JS

--- js/modernJS.js
+++ js/modernJS.js
@@ -18,5 +18,56 @@
 	// let a = +prompt("Первое число?", 1);
 	// let b = +prompt("Второе число?", 2);
 	// alert(a + b); // 12
+	// let 
+	// 	a = prompt('Введите первую цифру: '),
+	// 	b = prompt('Введите вторую цифру: ');
+	// alert(+a + +b);
+
+}
+
+// Сравнения (стр. 60) 
+{
+	// console.log('2' > '12');
+	// console.log(undefined == null);
+	// console.log(undefined === null);
+	// console.log(null === '\n0\n');
+}
+
+// Условное ветвление
+{
+	// Число 0 , пустая строка "" , null , undefined и NaN становятся false . Из-за этого
+	// их называют «ложными» («falsy») значениями.
+	// Остальные значения становятся true , поэтому их называют «правдивыми» («truthy»).
+	/*if ('0') alert('Вывелся');
+	let answer = prompt('Какое официальное название у JavaScript? ');
+	if (answer == 'ECMAScript') alert('Верно!');
+	else confirm('Не знаете ECMAScript?');*/
+
+	/*let answer = +prompt('Введите число: ');
+	if (answer > 0) {
+		alert(1);
+	}
+	else if (answer < 0) {
+		alert(-1);
+	}
+	else alert(0);*/
+
+	/*let 
+		result,
+		a = 1,
+		b = 2;
+	result = (a + b < 4) ? 'malo' : result = 'mnogo';
+	alert(result);*/
+
+	/*	let login = 'Сотрудник';
+		let message = (login == 'Сотрудник') ? 'Привет!' :
+			(login == 'Директор') ? 'Добрый день!' :
+				(login == '') ? 'Нет логина' :
+					'';
+		alert(message);*/
+}
+
+// Логические операторы (Стр. 66)
+{
 
 }
